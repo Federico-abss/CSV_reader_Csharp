868fd5a495793abce5cc8c97db87869655414a93
Federico-abss <54834159+Federico-abss@users.noreply.github.com>
2019-11-27 11:14:34 +0100

    Add files via upload
    Adding the project from the PC

---
+++ Calcolo Area/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Calcolo_Area
+{
+    public class WrongInputException : Exception
+    {
+        public WrongInputException(string message) : base(message)
+        {
+            Console.WriteLine("Forma non riconosciuta");
+        }
+    }
+    public class Calcolo
+    {
+        private Int32 a;
+        private Int32 b;
+        private string Forma;
+        public int Area(string forma, Int32 bas, Int32 altezza)
+        {
+            a = bas;
+            b = altezza;
+            Forma = forma;
+
+            switch (Forma) {
+                case "Quadrato":
+                case "Rettangolo":
+                    return a * b;
+                case "Triangolo":
+                    return a * b / 2;
+                default:
+                    throw (new WrongInputException("Forma non riconosciuta"));
+            }
+        }
+    }
+}
---
+++ Prova Pratica/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Calcolo_Area;
+
+namespace Prova_Pratica
+{    public struct Forma
+    {
+        public string nome;
+        public Int32 lato_base;
+        public Int32 altezza;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filename = @"C:\Users\corso6\Desktop\Prova Pratica\Prova Pratica.txt";
+            bool esiste = File.Exists(filename);
+            if (esiste)
+            {
+                List<Forma> forme = new List<Forma>();
+                string[] array = File.ReadAllLines(filename);
+
+                for (int i = 1; i < array.Length; ++i)
+                {
+                    var s = array[i];
+                    var record = s.Split(";");
+                    var nome = record[0];
+                    var lato_base = record[1];
+                    var altezza = record[2];
+
+                    var forma = new Forma
+                    {
+                        nome = nome,
+                        lato_base = Convert.ToInt32(lato_base),
+                        altezza = Convert.ToInt32(altezza)
+                    };
+                    forme.Add(forma);                    
+                }
+
+                Calcolo calcolo = new Calcolo();
+
+                foreach (var forma in forme)
+                {                   
+                    Int32 Area = calcolo.Area(forma.nome, forma.lato_base, forma.altezza);
+                    Console.WriteLine($"L'area del {forma.nome} è {Area}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dati non trovati");
+            }
+        }
+    }
+}
